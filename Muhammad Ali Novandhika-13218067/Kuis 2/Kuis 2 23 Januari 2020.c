ca1a594913c53f88de8c0584a74fd481210025d4
Novandhika <60213131+Novandhika@users.noreply.github.com>
2020-01-23 22:57:56 +0700

    Update Kuis 2 23 Januari 2020.c

--- Muhammad Ali Novandhika-13218067/Kuis 2/Kuis 2 23 Januari 2020.c
+++ Muhammad Ali Novandhika-13218067/Kuis 2/Kuis 2 23 Januari 2020.c
@@ -1,7 +1,7 @@
 /* Nama     :   Muhammad Ali Novandhika
 *  NIM      :   13218067
 *  Tanggal  :   23 Januari 2020
-*  Kuis 1
+*  Kuis 2
 
     Menyelesaikan persamaan linear
     y1 = 2x+2
@@ -18,14 +18,13 @@ int main() {
         m[2][1]={{-2},{4}},
         determinanD,determinanDx,determinanDy,x,y;                     /* deklarasi variabel */
 
-    determinanD=((k[0][0])*(k[1][1]))-((k[1][0])*(k[0][1]));
+    determinanD=((k[0][0])*(k[1][1]))-((k[1][0])*(k[0][1]));            // determinan dari variabel k
     determinanDx=((m[0][0])*(k[1][1]))-((m[1][0])*(k[1][0]));
     determinanDy=((k[0][0])*(m[1][0]))-((k[0][1])*(m[0][0]));
-    x=determinanDx/determinanD;
+    x=determinanDx/determinanD;                                         // Menggunakan cara Cramer
     y=determinanDy/determinanD;
     printf("%d\n",x);
     printf("%d",y);
-    //printf("%d",determinan);
 
     return 0;
 }
