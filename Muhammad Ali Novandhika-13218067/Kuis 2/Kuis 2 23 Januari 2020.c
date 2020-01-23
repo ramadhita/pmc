0aa46a92d8d0a6c4ab61fc7749f3e86885c4132f
Novandhika <60213131+Novandhika@users.noreply.github.com>
2020-01-23 22:54:14 +0700

    Add files via upload

---
+++ Muhammad Ali Novandhika-13218067/Kuis 2/Kuis 2 23 Januari 2020.c
@@ -0,0 +1,32 @@
+/* Nama     :   Muhammad Ali Novandhika
+*  NIM      :   13218067
+*  Tanggal  :   23 Januari 2020
+*  Kuis 1
+
+    Menyelesaikan persamaan linear
+    y1 = 2x+2
+    y2 = 4x-4
+
+*/
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int k[2][2]={{2,4},{-1,-1}},
+        m[2][1]={{-2},{4}},
+        determinanD,determinanDx,determinanDy,x,y;                     /* deklarasi variabel */
+
+    determinanD=((k[0][0])*(k[1][1]))-((k[1][0])*(k[0][1]));
+    determinanDx=((m[0][0])*(k[1][1]))-((m[1][0])*(k[1][0]));
+    determinanDy=((k[0][0])*(m[1][0]))-((k[0][1])*(m[0][0]));
+    x=determinanDx/determinanD;
+    y=determinanDy/determinanD;
+    printf("%d\n",x);
+    printf("%d",y);
+    //printf("%d",determinan);
+
+    return 0;
+}
+
