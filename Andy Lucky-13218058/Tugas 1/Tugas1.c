910033a2cba0a2d97ea93654dba5efc5602fe0d4
Lucranix28 <45935449+Lucranix28@users.noreply.github.com>
2020-01-29 02:45:24 +0700

    commit

--- Andy Lucky-13218058/Tugas 1/Tugas1.c
+++ Andy Lucky-13218058/Tugas 1/Tugas1.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include "Tugas1.h"
 
-int main(void) {
+int main(void)
+{
     int jumlah, n, i;
-    
-    do
+    int total_indeks[5] = {0, 0, 0, 0, 0};
+    char char_indeks[5] = "ABCDE";
     {
         /* code */
         printf("Jumlah data mahasiswa :  ");
         scanf(" %d", &jumlah);
-    } while (jumlah <=0);
+    }
+    while (jumlah <= 0)
+        ;
 
     data mahasiswa[jumlah];
-    
-    // Input Data 
+
+    // Input Data
     for (int i = 0; i < jumlah; i++)
     {
         /* code */
-        n = i+1;
+        n = i + 1;
         mahasiswa[i] = input(n);
     };
 
@@ -27,111 +30,24 @@ int main(void) {
         /* code */
         for (int j = i; j < jumlah; j++)
         {
-            if(mahasiswa[i].nilai < mahasiswa[j].nilai){
+            if (mahasiswa[i].nilai < mahasiswa[j].nilai)
+            {
                 swap(&mahasiswa[i], &mahasiswa[j]);
             };
         }
     };
 
     // Nilai tertinggi
-    int temp = mahasiswa[0].nilai, k = 0;
-    printf("Nilai tertinggi = %d, dengan nama : ", temp);
-    do
-    {
-        /* code */
-        printf(" %s", mahasiswa[k].name);
-        k += 1;
-        
-        if (mahasiswa[k].nilai >= temp)
-        {
-            printf(", ");
-        }
-        else
-        {
-            printf(". \n");
-        }
-        
-    } while (mahasiswa[k].nilai >= temp && k <= (jumlah-1));
+    max_nilai(mahasiswa, jumlah);
 
     // Nilai terendah
-    temp = mahasiswa[jumlah-1].nilai;
-    k = jumlah - 1 ;
-    printf("Nilai terendah = %d, dengan nama : ", temp);
-    do
-    {
-        /* code */
-        printf(" %s", mahasiswa[k].name);
-        k -= 1;
-
-        if (mahasiswa[k].nilai > temp || jumlah == 1)
-        {
-            printf(". \n");
-        }
-        else
-        {
-            printf(", ");
-        }
-
-    } while (mahasiswa[k].nilai <= temp && k > 0);
+    min_nilai(mahasiswa, jumlah);
 
     // Average
-    double average = 0;
-    for (int s = 0; s < jumlah; s++)
-    {
-        /* code */
-        average += mahasiswa[s].nilai;
-    }
-    average /= jumlah;
-    printf("Nilai rata-rata : %4.2lf \n", average);
-    
+    average(mahasiswa, jumlah);
 
     //Modulus Indeks
-    int total_indeks[5];
-    char inde[5] = {'A', 'B', 'C', 'D', 'E'};
-    for (int i = 0; i < jumlah; i++)
-    {
-        switch (mahasiswa[i].indeks)
-        {
-        case 'A':
-            /* code */
-            total_indeks[0] += 1;
-            break;
-        case 'B':
-            /* code */
-            total_indeks[1] += 1;
-            break;
-        case 'C':
-            /* code */
-            total_indeks[2] += 1;
-            break;
-        case 'D':
-            /* code */
-            total_indeks[3] += 1;
-            break;
-        case 'E':
-            /* code */
-            total_indeks[4] += 1;
-            break;
-        default:
-            break;
-        }
-    }
-    
-    int maks = 0;
-    for (int i = 0; i < 5; i++)
-    {
-        if(maks < total_indeks[i]){
-            maks = total_indeks[i];
-        }
-    }
-    
-    printf("Indeks Modulus dengan total : %d adalah :", maks);
+    modulus(mahasiswa, jumlah, total_indeks, char_indeks);
 
-    for (int i = 0; i < 5; i++)
-    {
-        if(maks = total_indeks[i]){
-            printf(" %c", inde[i]);
-        }
-    }
     return (0);
 }
--- Andy Lucky-13218058/Tugas 1/Tugas1.h
+++ Andy Lucky-13218058/Tugas 1/Tugas1.h
@@ -1,38 +1,50 @@
-#include<string.h>
+#include <string.h>
 
-typedef struct Data {
+typedef struct Data
+{
     char name[20];
     char indeks;
     int nilai;
 } data;
 
-void swap(data* a, data* b);
+struct ind
+{
+    char indeks;
+    int total;
+};
 
-char ind(int nilai){
-    if (nilai > 90)
+void swap(data *a, data *b);
+void max_nilai(data mahasiswa[], int jumlah);
+void min_nilai(data mahasiswa[], int jumlah);
+void average(data mahasiswa[], int jumlah);
+void modulus(data mahasiswa[], int jumlah, int total_indeks[], char char_indeks[]);
+
+char ind(int nilai)
+{
+    if (nilai >= 90)
     {
         /* code */
-        return 'A';
+        return ('A');
     }
-    else if (nilai > 80)
+    else if (nilai >= 80)
     {
         /* code */
-        return'B';
+        return ('B');
     }
-    else if (nilai > 70)
+    else if (nilai >= 70)
     {
         /* code */
-        return 'C';
+        return ('C');
     }
-    else if (nilai > 60)
+    else if (nilai >= 60)
     {
         /* code */
-        return 'D';
+        return ('D');
     }
     else
     {
         /* code */
-        return 'E';
+        return ('E');
     }
 }
 
@@ -40,35 +52,138 @@ data input(int n)
 {
     /* data */
     data a;
-    printf("    Nama mahasiswa %d :  ", n);
+    printf("    Nama mahasiswa %d    :  ", n);
     scanf(" %[^\n]%*c", &a.name);
-    
+
     do
     {
         /* code */
-        printf("    Nilai           :   ");
+        printf("    Nilai               :  ");
         scanf(" %d", &a.nilai);
 
-    } while (a.nilai < 0 || a.nilai >100);
+    } while (a.nilai < 0 || a.nilai > 100);
 
     a.indeks = ind(a.nilai);
+    printf("    Indeks              : %c \n", a.indeks);
     printf("\n");
-    return(a);
+    return (a);
 };
 
-
 // swap
-void swap(data* a, data* b)
+void swap(data *a, data *b)
 {
-    data  t = *a;
+    data t = *a;
     *a = *b;
     *b = t;
 }
 
+void max_nilai(data mahasiswa[], int jumlah)
+{
+    int temp = mahasiswa[0].nilai, k = 0;
+    printf("Nilai tertinggi = %d, dengan nama : ", temp);
+    do
+    {
+        /* code */
+        printf(" %s", mahasiswa[k].name);
+        k += 1;
 
+        if (mahasiswa[k].nilai < temp || jumlah == 1)
+        {
+            printf(". \n");
+        }
+        else
+        {
+            printf(", ");
+        }
 
+    } while (mahasiswa[k].nilai >= temp && k < (jumlah - 1));
+}
+
+void min_nilai(data mahasiswa[], int jumlah)
+{
+    int temp = mahasiswa[jumlah - 1].nilai, k = jumlah - 1;
+    printf("Nilai terendah = %d, dengan nama : ", temp);
+    do
+    {
+        /* code */
+        printf(" %s", mahasiswa[k].name);
+        k -= 1;
+
+        if (mahasiswa[k].nilai > temp || jumlah == 1)
+        {
+            printf(". \n");
+        }
+        else
+        {
+            printf(", ");
+        }
+
+    } while (mahasiswa[k].nilai <= temp && k > 0);
+}
+
+void average(data mahasiswa[], int jumlah)
+{
+    double average = 0;
+    for (int i = 0; i < jumlah; i++)
+    {
+        /* code */
+        average += mahasiswa[i].nilai;
+    }
+    average /= jumlah;
+    printf("Nilai rata-rata : %4.2lf \n", average);
+}
+
+void modulus(data mahasiswa[], int jumlah, int total_indeks[], char char_indeks[])
+{
+    for (int i = 0; i < jumlah; i++)
+    {
+        switch (mahasiswa[i].indeks)
+        {
+        case 'A':
+            /* code */
+            total_indeks[0] += 1;
+            break;
+        case 'B':
+            /* code */
+            total_indeks[1] += 1;
+            break;
+        case 'C':
+            /* code */
+            total_indeks[2] += 1;
+            break;
+        case 'D':
+            /* code */
+            total_indeks[3] += 1;
+            break;
+        case 'E':
+            /* code */
+            total_indeks[4] += 1;
+            break;
+        default:
+            break;
+        }
+    }
+
+    int max = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (max < total_indeks[i])
+        {
+            max = total_indeks[i];
+        }
+    }
+
+    printf("Nilai modulus dengan frekuensi %d, yaitu : ", max);
+    for (int i = 0; i < 5; i++)
+    {
+        if (total_indeks[i] == max)
+        {
+            printf(" %c", char_indeks[i]);
+        }
+    }
+}
 // void data(struct data mahasiswa[5]){
-    
+
 //     /* data */
 //     mahasiswa[0].name = "Mawar";
 //     mahasiswa[0].nilai = 67;
@@ -77,7 +192,7 @@ void swap(data* a, data* b)
 //     mahasiswa[1].name = "Melati";
 //     mahasiswa[1].nilai = 85;
 //     mahasiswa[1].indeks = 'A';
-    
+
 //     mahasiswa[2].name = "Wisteria";
 //     mahasiswa[2].nilai = 85;
 //     mahasiswa[2].indeks = 'A';
@@ -85,9 +200,28 @@ void swap(data* a, data* b)
 //     mahasiswa[3].name = "Carnation";
 //     mahasiswa[3].nilai = 70;
 //     mahasiswa[3].indeks = 'B';
-    
+
 //     mahasiswa[4].name = "Lili";
 //     mahasiswa[4].nilai = 60;
 //     mahasiswa[4].indeks = 'D';
 // };
 //     };
+
+// int maks = 0;
+// for (int i = 0; i < 5; i++)
+// {
+//     if (maks < total_indeks[i])
+//     {
+//         maks = total_indeks[i];
+//     }
+// }
+
+// printf("Indeks Modulus dengan total : %d adalah :", maks);
+
+// for (int i = 0; i < 5; i++)
+// {
+//     if (maks = total_indeks[i])
+//     {
+//         printf(" %c", inde[i]);
+//     }
+// }
