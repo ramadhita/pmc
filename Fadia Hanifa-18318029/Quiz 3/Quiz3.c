1063111d1bc4a17c6d3277d0009fa1daabb87182
fadiahanifa <60212665+fadiahanifa@users.noreply.github.com>
2020-02-06 12:47:01 +0700

    Add files via upload

--- Fadia Hanifa-18318029/Quiz 3/Quiz3.c
+++ Fadia Hanifa-18318029/Quiz 3/Quiz3.c
@@ -2,32 +2,28 @@
 // Fadia Hanifa Suwandoko / 18318029
 // Hitung luas lingkaran dengan integral
  
-// Inisiasi Konstanta
-float pi = 3.14159;
-int r = 7;
-float a = 0;
-float c;
-float sum1 = 0;
-float sum2 = 0;
-float i, j;
-float luas;
-float n = 70000;
-float galat;
-// Program Utama
 int main() {
-    c = 2 * pi;                             // batas atas integral kedua
-    float deltax = r/n;                     // delta integral pertama
+    // inisialisasi
+    int b = 7;
+    int a = 0;
+    float pi = 3.14159;
+    float i, j;
+    float c = 2 * pi;                       // batas atas integral kedua
+    float n = 70000;
+    float deltax = b/n;                     // delta integral pertama
     float deltay = c/n;                     // delta integral kedua
     // Integral Pertama (integral r)
-    for (i = a; i <= r; i = i + deltax){
+    float sum1 = 0;
+    for (i = a; i <= b; i = i + deltax){
         sum1 = sum1 + (i * deltax);         // sum1 = jumlah riemann f(x) = x, dx = (r/n)
     }
     // Integral Kedua (integral theta)
+    float sum2 = 0;
     for (i = a; i <= c; i = i + deltay){
         sum2 = sum2 + (sum1*deltay);        // sum2 = jumlah reimann f(x) = sum1, dy = (c/n)
     }
-    luas = pi * r * r;                      // luas dengan rumus biasa
-    galat = ((sum2 - luas)/luas) * 100;     // perbedaan luas dengan rumus dan dengan jumlah riemann
+    float luas = pi * b * b;                      // luas dengan rumus biasa
+    float galat = ((sum2 - luas)/luas) * 100;     // perbedaan luas dengan rumus dan dengan jumlah riemann
     printf("LUAS 1 = %3.2f\n", sum2);       // Display luas dan galat
     printf("LUAS 2 = %3.2f\n", luas);
     printf("Galat = %f persen\n", galat);
