e4e78f814b9882b77068f2510fc90fb5a12a5a76
davidf1000 <davidfauzi29@gmail.com>
2020-02-15 06:21:44 +0700

    tambahin source flowchat laporan quiz4

--- David Fauzi_13218043/Quiz 4/David Fauzi_13218043_Quiz4.c
+++ David Fauzi_13218043/Quiz 4/David Fauzi_13218043_Quiz4.c
@@ -7,15 +7,15 @@ double r1=10000;
 double c1=0.000001;
 double v1=5;
 int t=1;
-double arrayvo[500];
-double dt=0.0001;
+double arrayvo[100];
+double dt=0.001;
 
 int main()
 {
     // ASUMSI KONDISI AWAL Vo=0
     arrayvo[0]=0;
     // Masukan nilai Vo untuk setiap selang waktu dt=0.0001
-    while (t<500)
+    while (t<100)
     {
         arrayvo[t]=((v1+(arrayvo[t-1]*r1*c1/dt))/ (1+(r1*c1/dt)));
         t++;
@@ -24,7 +24,7 @@ int main()
     FILE *x;
     // Buka File
     x=fopen("data.txt","w");
-    for (int i = 0; i < 500; i++)
+    for (int i = 0; i < 100; i++)
     {
         // Masukan nilai didalam Array ke dalam .txt
         printf("%lf \n",arrayvo[i]);
