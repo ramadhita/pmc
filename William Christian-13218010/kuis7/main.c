d1440419862a180dfcab3e93fe24a9f1d202dfa5
wcgodjali <60223148+wcgodjali@users.noreply.github.com>
2020-02-29 12:21:30 +0700

    Add files via upload

---
+++ William Christian-13218010/kuis7/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    // Preparation and Initialization
+    FILE * fp = fopen("heightvstime.csv", "w+");
+    float time ;
+    double height;
+    double tetha;
+    double radius = 0.025;
+    double tembereng;
+    double height_out;
+    fprintf(fp, "waktu (s), Tinggi (m)\n");
+    for (time = 0; time <= 11.5; time += 0.1){
+        height += 0.0001 ;
+        if (height < radius){
+            tetha = acos((radius - height)/radius)* 2;
+            tembereng = (tetha/2*pow(radius,2)) - (0.5 * pow(radius,2)* sin(tetha));
+            height_out = sqrt(2*10*(100-height)) / 10 * tembereng;
+        }
+        else if (height == radius){
+            height_out = sqrt(2*10*(100-height)) / 10 * 0.5 * M_PI * pow(radius,2);
+        }
+        else {
+            tetha = acos((height-radius)/radius) * 2;
+            tembereng = (tetha/2*pow(radius,2)) - (0.5 * pow(radius,2)* sin(tetha));
+            height_out = sqrt(2*10*height) / 10 *(M_PI*pow(radius,2) - tembereng);
+        }
+        height -= height_out;
+        fprintf(fp, "%f, %f\n", time, height);
+    }
+    return 0;
+}
