972e935c2452f762c81520a67936671e37faafc3
salmajidah <60214238+salmajidah@users.noreply.github.com>
2020-03-08 00:16:05 +0700

    Update Salma Majidah_18318010_Kode Program.c

--- Salma Majidah_18318010/Kuis 8/Salma Majidah_18318010_Kode Program.c
+++ Salma Majidah_18318010/Kuis 8/Salma Majidah_18318010_Kode Program.c
@@ -46,12 +46,12 @@ int main (){
 
 	
 	//Mengeluarkan matriks hasil penukaran ke layar
-	for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
+for(i = 0; i < 10; i++) {
+	for(j = 0; j < 10; j++) {
             printf("%d\t", A[i][j]);
         }
         printf("\n");
-    }
+}
     
 	return 0;
 }
