ca154386844f590adca0c8a95136242b918fb791
syarifatull <60220712+syarifatull@users.noreply.github.com>
2020-03-09 00:43:38 +0700

    Add files via upload

---
+++ Syarifatul Lathifah-18318016/Kuis 8/matriks.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int x, y, temp, A[10][10];                      //inisialisasi variabel
+
+    for (x=0; x<=9; x++)                            //iterate baris
+    {
+        for (y=0; y<=9 ; y++)                       //iterate kolom
+        {
+            A[x][y] = (rand()%(21)-10);             //matriks A berisi nilai random dari -10 hingga 10
+        }
+    }
+
+    for (x=0; x<=9; x++)                            //iterate baris
+    {
+        for (y=0; y<=9 ; y++)                       //iterate kolom
+        {
+            printf("%d\t", A[x][y]);                //print matriks awal
+            if (y == 9)                             //ketika kolom sudah mencapai indeks 9
+            {
+                printf("\n");                       //maka enter
+            }
+        }
+    }
+
+    printf("\n");
+
+    for (x=0; x<=4; x++)                            //iterate baris 1-5
+    {
+        for (y=x+1; y<=(8-x); y++)                  //iterate kolom
+        {
+            temp = A[x][y];                         //temp untuk menyimpan nilai sementara dari matriks
+            A[x][y] = A[9-x][y];                    //tukar nilai atas dengan bawah
+            A[9-x][y] = temp;
+        }
+    }
+
+    for (x=0; x<=4; x++)                            //iterate baris 1-5
+    {
+        for (y=x+1; y<=(8-x); y++)                  //iterate kolom
+        {
+            temp = A[y][x];                         //temp untuk menyimpan nilai sementara dari matriks
+            A[y][x] = A[y][9-x];                    //tukar nilai kiri dengan kanan
+            A[y][9-x] = temp;
+        }
+    }
+
+    for (x=0; x<=9; x++)                            //iterate baris 1-9
+    {
+        for (y=0; y<=9 ; y++)                       //iterate kolom 1-9
+        {
+            printf("%d\t", A[x][y]);                //print matriks yang berhasil diswap
+        }
+
+        printf("\n");
+    }
+}
