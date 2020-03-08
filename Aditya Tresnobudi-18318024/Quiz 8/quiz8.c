aac0d351f779d22c5dda83e3a4c7e0517b51bc88
adityatresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2020-03-08 20:30:19 +0700

    Add files via upload

---
+++ Aditya Tresnobudi-18318024/Quiz 8/quiz8.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void)
+{
+    int A[10][10];
+    int B[10][10];
+    int i, j;
+    srand(time(0));
+    printf("Matrix Awal : \n");
+    for(i = 0; i < 10; i++)
+    {
+        for(j = 0; j < 10; j++)
+        {
+            A[i][j] = rand() % (10 + 1 + 10) - 10;
+            printf("%3d ", A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(i = 0; i < 10; i++)
+    {
+        for(j = 0; j < 10; j++)
+        {
+            if(i <= 3 && j >= i + 1 && j <= 8-i)
+            {
+                B[9-i][j] = A[i][j];
+            }
+            else if(i >= 6 && j >= (9-i) + 1 && j <= i-1)
+            {
+                B[9-i][j] = A[i][j];
+            }
+            else if(j <= 3 && i >= j + 1 && i <= 8-j)
+            {
+                B[i][9-j] = A[i][j];
+            }
+            else if(j >= 6 && i >= (10-j) && i <= j-1)
+            {
+                B[i][9-j] = A[i][j];
+            }
+            else
+            {
+                B[i][j] = A[i][j];
+            }
+        }
+    }
+    printf("Matrix Relokasi : \n");
+    for(i = 0; i < 10; i++)
+    {
+        for(j = 0; j < 10; j++)
+        {
+            printf("%3d ",B[i][j]);
+        }
+        printf("\n");
+    }
+}
