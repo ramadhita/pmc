d3df1ce3341c91fbd985d6c184f666ba502ca5f8
pradiptaw <60214980+pradiptaw@users.noreply.github.com>
2020-03-22 18:14:21 +0700

    Add files via upload

---
+++ Pradipta Wasundari - 18318017/Kuis 1/PradiptaWasundari_18318017_Kuis1.c
@@ -0,0 +1,30 @@
+/* 
+Nama (NIM)			: Pradipta Wasundari (18318017)
+Deskripsi			: Parabola
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{   
+	double x, y, Voy, Vox, t, xmax;    
+
+	t = 0;
+	x = 0;
+	y = 0;
+	Vox = 50*0.5;
+	Voy = 50*0.87;
+	xmax = (2*Vox*Voy)/10;
+	
+	while (y>=0)
+{
+	printf("Bola berada pada posisi %lf, %lf\n", x, y);
+	t = t+0.01;
+	y = (Voy*t) - (0.5*10*t*t);
+	x = Vox*t;
+}
+	printf("Jarak terjauh yang ditempuh bola adalah %lf meter", xmax);
+	
+	return(0);
+}
